f677929333413780731d3356cf411dc28ef7dd32
thisisrv <50019337+thisisrv@users.noreply.github.com>
2021-12-16 23:32:04 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+# Replace 'Friend's Name' with the name of your friend
+# or the name of a group
+target = '"contact_name"'
+
+# Replace the below string with your own message
+string = "message text"
+
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located((
+	By.XPATH, x_arg)))
+group_title.click()
+inp_xpath = '//div[@class="_13NKt copyable-text selectable-text"][@data-tab="9"]'
+input_box = wait.until(EC.presence_of_element_located((
+	By.XPATH, inp_xpath)))
+for i in range(100):
+	input_box.send_keys(string + Keys.ENTER)
+	time.sleep(1)
